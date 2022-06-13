b52eb53b46a04c5b381ea318d7e3e73d56c7f19f
Karen Plagemann <karplagemann@gmail.com>
2022-06-13 16:48:49 -0300

    Update coderhouse.js

--- coderhouse.js
+++ coderhouse.js
@@ -1,5 +1,21 @@
 alert ("Bienvenido a la creacion de un producto")
-let tituloProducto = prompt("Ingrese el titulo de su producto")
 
-let titulo = document.getElementById('titulo')
+let tituloProducto = prompt("Ingrese el titulo de su producto")
+let titulo = document.getElementById("El titulo de su producto sera: " + titulo)
 titulo.innerHTML = tituloProducto
+
+let descripcionProducto = prompt("Ingrese una descripcion para su producto")
+let descripcion = document.getElementById("La descripcion de su producto sera: " + descripcionProducto)
+titulo.innerHTML = descripcionProducto
+
+let precioProducto = prompt("Ingrese un precio para su producto")
+let precio = document.getElementById("Su preco es $: " + precioProducto)
+function solicitarValor() {
+  precioProducto = Number(prompt("Ingrese el valor a simular"));
+  while (isNaN(precioProducto) || "" == precioProducto) {
+    precioProducto = Number(prompt("Por favor ingrese un valor valido"));
+  }
+  return precio;
+}
+solicitarValor();
+titulo.innerHTML = precioProducto
